8e98c8e9f78d264600dfce1869541a63c6e3a106
benzmugi <mugiyuiyui@gmail.com>
2019-06-10 09:56:23 +0900

    課題提出

---
+++ Assets/Lesson5 (3)/Lesson5/ScoreText.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+
+    private int score = 0;
+    private GameObject scoreText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+        score = 0; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 40;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 30;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 20;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 10;
+        }
+        String str = score.ToString();
+        this.scoreText.GetComponent<Text>().text = str;
+    }
+}
+
